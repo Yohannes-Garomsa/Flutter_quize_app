be8241231b7b3796928a3981852515d23f46049f
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-04-17 10:51:12 +0300

    home scree

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quiz_app/providers/quiz_provider.dart';
+import 'package:quiz_app/screens/quiz_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Provider = Provider.of<QuizProvider>(context);
+    return Scaffold(
+      appBar: AppBar(title: const Text('Quiz Ethiopia')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to Quiz Ethiopia!',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Test your knowledge about Ethiopia and the world with our fun quiz! Click the button below to start.',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                Provider.resetQuize();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const quize_screen()),
+                );
+              },
+              child: const Text('Start Quiz'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
